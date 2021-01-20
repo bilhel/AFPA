b1ac2a9de1dc83c484b9eab11f955f414bf0bc13
bilhel <torche.bilhel@gmail.com>
2021-01-20 15:29:14 +0100

    Exo JS OK

--- Bilhel/FRONT/DYNAMIQUE/JAVASCRIPT/formulaire.js
+++ Bilhel/FRONT/DYNAMIQUE/JAVASCRIPT/formulaire.js
@@ -1,48 +1,150 @@
-// var nom = document.getElementById("nom")
-// var prenom = document.getElementById("prenom")
-// var sexe1 = document.getElementById("Sexe1")
-// var sexe2 = document.getElementById("Sexe2")
-// var sexe3 = document.getElementById("Sexe3")
-// // var ddn = document.getElementById("Date_de_naissance")
-// var cp = document.getElementById("Code_Postal")
-// var email = document.getElementById("Email")
-// var sujet = document.getElementById("Sujet")
-// var question = document.getElementById("Question")
-// var submit = document.getElementById('submit');
+var check = document.getElementById('envoie');
+check.addEventListener("click", function verif(event) {
+    var Identité = /(^[A-Z]+[a-zA-Zéèêëôœîïûüàáâæç-\s]+$)/;
+    var mail = /(^[\w\.-]+@[a-z]+[\.]{1}[a-z]{2,3}$)/;
+    var birth = /(^[0-9]{4}[\-]{1}[0-9]{2}[\-]{1}[[0-9]{2}$)/;
+    var cp = /(^[0-9]{5}$)/;
+    var adr = /(^[0-9]+[a-zA-Z-\s]+$)|^$/;
+    var Localisation = /(^[A-Z]+[a-zA-Zéèêëôœîïûüàáâæç-\s]+$)|^$/;
 
-// var missNom = document.getElementById('missNom');
-// var missPrenom = document.getElementById('missPrenom');
-// // var missDdn = document.getElementById('missDdn');
-// var missCP = document.getElementById('missCP');
-// var missEmail = document.getElementById('missEmail');
-// var missQuestion = document.getElementById('missQuestion');
+    var nom = document.getElementById("nom").value;
+    var prenom = document.getElementById("prenom").value;
+    var sexe1 = document.getElementById("inlineRadio1").checked;
+    var sexe2 = document.getElementById("inlineRadio2").checked;
+    var sexe3 = document.getElementById("inlineRadio3").checked;
+    var date = document.getElementById("date").value;
+    var codepostal = document.getElementById("codepostal").value;
+    var adresse = document.getElementById("adresse").value;
+    var ville = document.getElementById("ville").value;
+    var email = document.getElementById("mail").value;
+    var sujet = document.getElementById("sujet").value;
+    var accept = document.getElementById("accept").checked;
+    var question = document.getElementById("question").value;
+    
+    if (!Identité.test(nom))
+    {
+        document.getElementById("erreurName").textContent = "Utilisez uniquement des caractères alphabétiques";
+        document.getElementById("erreurName").style.color = 'red';
+        event.preventDefault();  
+    }
+    else
+    {
+        document.getElementById("erreurName").textContent = "Valide !";
+        document.getElementById("erreurName").style.color = 'green';
+        event.preventDefault();
+    }
+    
+    if (!Identité.test(prenom))
+    {
+        document.getElementById("erreurPrenom").textContent = "Utilisez uniquement des caractères alphabétiques";
+        document.getElementById("erreurPrenom").style.color = 'red';
+        event.preventDefault();
+    }
+    else
+    {
+        document.getElementById("erreurPrenom").textContent = "Valide !";
+        document.getElementById("erreurPrenom").style.color = 'green';
+        event.preventDefault();
+    }
 
-// var textValid = /^[a-zA-Z\-\déèàçùëüïôäâêûîô#&]+$/;
-// var postalCodeValid = /^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B))[0-9]{3}$/;
-// var emailValid = /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/;
+    if (sexe1 == false && sexe2 == false && sexe3 == false)
+    {
+        document.getElementById("erreurSexe").textContent = "Veuillez choisir une réponse";
+        document.getElementById("erreurSexe").style.color = 'red';
+        event.preventDefault();
+    }
+    else
+    {
+        document.getElementById("erreurSexe").textContent = "Valide !";
+        document.getElementById("erreurSexe").style.color = 'green';
+        event.preventDefault();
+    }
 
-// nom.onblur = nomCheck;
-// prenom.onblur = prenomCheck;
-// cp.onblur = cpCheck;
-// email.onblur = emaikCheck;
-// sujet.onblur = sujetCheck;
-// question.onblur = questionCheck;
+    if (!birth.test(date))
+    {
+        document.getElementById("erreurDate").textContent = "Date de naissance non conforme";
+        document.getElementById("erreurDate").style.color = 'red';
+        event.preventDefault();
+    }
+    else
+    {
+        document.getElementById("erreurDate").textContent = "Valide !";
+        document.getElementById("erreurDate").style.color = 'green';
+        event.preventDefault();
+    }
 
-// function nomCheck() {
-//     // si le champ est vide
-//     if (nom.validity.valueMissing) {
-//         event.preventDefault()
-//         missNom.textContent = 'Champ non renseigné';
-//         missNom.style.color = 'red';
-//         // test de la validité du champs de saisie
-//     } else if (textValid.test(nom.value) == false) {
-//         event.preventDefault();
-//         missNom.textContent = 'Format incorrect';
-//         missNom.style.color = 'red';
-//         // si la saisie est correct
-//     } else {
+    if (!cp.test(codepostal))
+    {
+        document.getElementById("erreurCP").textContent = "Code Postal non conforme";
+        document.getElementById("erreurCP").style.color = 'red';
+        event.preventDefault();
+    }
+    else
+    {
+        document.getElementById("erreurCP").textContent = "Valide !";
+        document.getElementById("erreurCP").style.color = 'green';
+        event.preventDefault();
+    }
 
-//         missNom.textContent = 'Ok';
-//         missNom.style.color = 'green';
-//     }
-// };
+    if (!adr.test(adresse)) {
+        document.getElementById("erreurAdr").textContent = "Adresse non conforme";
+        document.getElementById("erreurAdr").style.color = 'red';
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("erreurAdr").textContent = "Valide !";
+        document.getElementById("erreurAdr").style.color = 'green';
+        event.preventDefault();
+    }
+    if (!Localisation.test(ville)) {
+        document.getElementById("erreurVille").textContent = "Ville non conforme";
+        document.getElementById("erreurVille").style.color = 'red';
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("erreurVille").textContent = "Valide !";
+        document.getElementById("erreurVille").style.color = 'green';
+        event.preventDefault();
+    }
+    if (!mail.test(email)) {
+        document.getElementById("erreurMail").textContent = "Adresse mail non conforme";
+        document.getElementById("erreurMail").style.color = 'red';
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("erreurMail").textContent = "Valide !";
+        document.getElementById("erreurMail").style.color = 'green';
+        event.preventDefault();
+    }
+    if (sujet == 1)
+    {
+        document.getElementById("erreurSujet").textContent = "Selectionnez le sujet de votre demande";
+        document.getElementById("erreurSujet").style.color = 'red';
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("erreurSujet").textContent = "Valide !";
+        document.getElementById("erreurSujet").style.color = 'green';
+        event.preventDefault();
+    }
+    if (accept != true) {
+        document.getElementById("erreurAccept").textContent = "Veuillez cocher cette case";
+        document.getElementById("erreurAccept").style.color = 'red';
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("erreurAccept").textContent = "Valide !";
+        document.getElementById("erreurAccept").style.color = 'green';
+        event.preventDefault();
+    }
+    if (question == "") {
+        document.getElementById("erreurQuest").textContent = "Ce champ est obligatoire";
+        document.getElementById("erreurQuest").style.color = 'red';
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("erreurQuest").textContent = "Valider";
+        document.getElementById("erreurQuest").style.color = 'green';
+        event.preventDefault();
+    }
+});
